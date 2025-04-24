2410075c0daccc7c626b7f7470d3a498f944dadf
Amit Parajuli <amitparajuli077@gmail.com>
2025-04-24 22:02:56 +0545

    new commit

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 /** @format */
+/** @format */
+let openmodal = document.getElementById("open");
+let closemodal = document.getElementById("close");
+let modal = document.getElementById("user");
+let submit = document.querySelector(".submit-btn");
+
+//open
+openmodal.addEventListener("click", () => {
+  modal.classList.add("show-modal");
+});
+// close
+closemodal.addEventListener("click", () => {
+  modal.classList.remove("show-modal");
+});
+//modal
+window.addEventListener("click", (e) => {
+  e.target === modal ? modal.classList.remove("show-modal") : false;
+});
+
+$(document).ready(function () {
+  $(".open").click(function (e) {
+    $(".user-idea").fadeIn(500);
+    $(".modal").addClass("transform-in");
+  });
+});
